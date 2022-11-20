67be7a34db85ee07fa7106b48187b239b9ad45ff
unknown <jihanfadilah330@gmail.com>
2022-11-20 14:55:54 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 export const API = axios.create({
   // baseURL: process.env.REACT_APP_BASEURL,
   // jihan:
-  // baseURL: 'https://waysfood-go.herokuapp.com/api/v1', 
+  baseURL: 'https://waysfood-go.herokuapp.com/api/v1', 
   
-  baseURL: 'https://be-waysfood.herokuapp.com/api/v1',
+  // baseURL: 'https://be-waysfood.herokuapp.com/api/v1',
 });
 
 export const setAuthToken = (token) => {
